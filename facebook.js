93fe136125c6e5097ef8c1b4695315f6dee34a18
Jack Dearlove <jackdearlove@users.noreply.github.com>
2016-11-12 14:49:23 +0000

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -26,6 +26,18 @@ const fbMessage = (recipientId, msg, cb) => {
       },
       message: {
         text: msg,
+       quick_replies:[
+      {
+        "content_type":"text",
+        "title":"Red",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+      },
+      {
+        "content_type":"text",
+        "title":"Green",
+        "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+      }
+    ]
       },
     },
   };
